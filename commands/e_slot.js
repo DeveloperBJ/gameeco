0becb6671d08803e61271cd07bfbb5d96d328de0
DeveloperBJ <71545857+DeveloperBJ@users.noreply.github.com>
2020-09-21 21:47:34 +0530

    prefix

--- commands/e_slot.js
+++ commands/e_slot.js
@@ -10,8 +10,8 @@ const db = eco.mongo();
 module.exports.run = async (bot, message, args) => {
 
     const guildPrefix = await eco.fetch(`prefix_${message.guild.id}`)
-    if(guildPrefix === null) guildPrefix = config.prefix;
-    if(!message.content.startsWith(guildPrefix))return;  
+      var prefix = (!guildPrefix) ? config.prefix : guildPrefix;
+    if(!message.content.startsWith(prefix))return;  
     function getHexColor(){
         let mColor = message.member.displayHexColor;
         if(mColor == null){
