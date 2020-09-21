b79c0802eab208c399021c65fd325fb24f6cc9d4
DeveloperBJ <71545857+DeveloperBJ@users.noreply.github.com>
2020-09-21 14:13:54 +0530

    added mongodb removed mysql

--- commands/e_store.js
+++ commands/e_store.js
@@ -1,122 +1,34 @@
-const Discord = require('discord.js')
-const db = require('quick.db')
-const config = require('../config.json')
-const Prefix = config.prefix;
-const mysql = require('mysql');
-
-
-var con = mysql.createConnection({
-    host: process.env.host,
-    user: process.env.user,
-    password: process.env.password,
-    database: process.env.database
-});
+require('dotenv').config()
+const Discord = require('discord.js');
+const { Ecobase } = require('mongo.eco')
+const config = require('../config.json');
+const eco = new Ecobase(process.env.MONGO)
+const db = eco.mongo();
 
 module.exports.run = async (bot, message, args) => {
-    if(!message.content.startsWith(Prefix))return;  
-   function getHexColor(){
-    let mColor = message.member.displayHexColor;
-    if(mColor == null){
-      return config.primary;
-    }else{
-      return mColor;
-    }
-   }
-  
-     con.connect(function (err) {
-    if (err) throw err;
-    console.log('database connected successfully :)');
 
-    var sql = `CREATE TABLE IF NOT EXISTS store(id INT(20) AUTO_INCREMENT PRIMARY KEY, item VARCHAR(255), content VARCHAR(255), status VARCHAR(20) DEFAULT 'YES')`;
-    con.query(sql, function (err, result) {
-        if (err) throw err;
-        console.log("TABLE named 'store' successfully created !");
-    });
-      
-    function getHexColor(){
-    let mColor = message.member.displayHexColor;
-    if(mColor == null){
-      return config.primary;
-    }else{
-      return mColor;
-    }
-   }
-    
-    con.query("SELECT * FROM store", function (err, result) {
-        if (err) throw err;
-      
-//         for (let i = 0; i < result.length; i++) {
-       function stockItem(){
-         if(result[0].status === "YES"){
-           return 'cost: 9500 Points';
-         }else{
-           return 'out of stock';
-         }
-       }
-      function stockItem1(){
-         if(result[1].status === "YES"){
-           return 'cost: 9500 Points';
-         }else{
-           return 'out of stock';
-         }
-       }
-      function stockItem2(){
-         if(result[2].status === "YES"){
-           return 'cost: 9500 Points';
-         }else{
-           return 'out of stock';
-         }
-       }
-      function stockItem3(){
-         if(result[3].status === "YES"){
-           return 'cost: 9500 Points';
-         }else{
-           return 'out of stock';
-         }
-       }
-      function stockItem4(){
-         if(result[4].status === "YES"){
-           return 'cost: 9500 Points';
-         }else{
-           return 'out of stock';
-         }
-       }
-      function stockItem5(){
-         if(result[5].status === "YES"){
-           return 'cost: 9500 Points';
-         }else{
-           return 'out of stock';
-         }
-       }
-          
-           let embed = new Discord.MessageEmbed()
-           .setTitle(`**${bot.user.username.toUpperCase()} GAME STORE ━━━━━ **`)
-           .setDescription('\n use command to purchase an item for example - `f!buy 1` . These items will changed time to time. \n\n')
-           .setThumbnail('https://my.games/hotbox/mygames/media/news/0376e83f545c5626a902201c4aa504f7.png')
-           .addField(`${result[0].id}. ${result[0].item}`, "`" + ` ${stockItem()} ` + "`" , true)
-           .addField(`${result[1].id}. ${result[1].item}`, "`" + ` ${stockItem1()} ` + "`" , true)           
-           .addField(`${result[2].id}. ${result[2].item}`, "`" + ` ${stockItem2()} ` + "`" , true)           
-           .addField(`${result[3].id}. ${result[3].item}`, "`" + ` ${stockItem3()} ` + "`" , true)
-           .addField(`${result[4].id}. ${result[4].item}`, "`" + ` ${stockItem4()} ` + "`" , true)
-           .addField(`${result[5].id}. ${result[5].item}`, "`" + ` ${stockItem5()} ` + "`" , true)
-           .setColor(getHexColor())
-           .setTimestamp()
-           .setFooter(`Requested by ${message.author.tag}`, message.author.displayAvatarURL());
-            message.channel.send(embed)
-          // }
-          
-      });
-     con.end();
-  });
+    const guildPrefix = await eco.fetch(`prefix_${message.guild.id}`)
+    if(guildPrefix === null) guildPrefix = config.prefix;
 
+    if(!message.content.startsWith(guildPrefix))return;  
+    let user = message.author;
 
+    let author = eco.fetch(`${message.author.id}`)
+
+    let storeEmbed = new Discord.MessageEmbed()
+    .setTitle(`**${bot.user.username.toUpperCase()}**'S STORE -----`)
+    .addField(`1.Game Coupon`, `cost: 9600 coins`, true)
+    .addField(`2.GTA 5`,`cost: 9600 coins`, true)
+    .addField(`3.PayDay 2`, `cost: 11500 coins`, true)
+
+    message.channel.send(storeEmbed);
 
 }
-
 module.exports.help = {
     name: "store",
-    type: "fun",
-    usage: "`store`",
-    about: "display store items",
-    aliases: ["shop"]
-  };
+    type:"fun",
+    usage:"`store` or  `shop` or `market`",
+    about:"Use the command to display store items!",
+    aliases: ["market"],
+  
+  }
