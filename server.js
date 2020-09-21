152db8993a9b9fbd970bcfd08ef767260bcb09fd
DeveloperBJ <71545857+DeveloperBJ@users.noreply.github.com>
2020-09-21 15:03:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -67,8 +67,8 @@ fs.readdir("./commands/", (err, files) => {
   
     bot.on("message", async message => {
       if(!message.guild) return;
-      let guildPrefix = await eco.fetch(`prefix_${message.guild.id}`)
-      if(guildPrefix == null) return guildPrefix = config.prefix;
+      const guildPrefix = await eco.fetch(`prefix_${message.guild.id}`)
+      if(guildPrefix == null) return config.prefix;
 
       if (message.author.bot) return;
       if (message.channel.type === "dm") return;
