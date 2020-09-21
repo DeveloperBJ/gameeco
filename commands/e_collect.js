ec1f1eb199ecaa0e628cd5cb1e662b660b58c8b6
DeveloperBJ <71545857+DeveloperBJ@users.noreply.github.com>
2020-09-21 14:07:34 +0530

    mongodb added

--- commands/e_collect.js
+++ commands/e_collect.js
@@ -1,49 +1,64 @@
+require('dotenv').config()
 const Discord = require("discord.js");
-const db = require("quick.db");
 const ms = require("parse-ms");
-const config = require('../config.json')
-const Prefix = config.prefix;
-module.exports.run = async (bot, message, args) => {
-  if(!message.content.startsWith(Prefix))return;  
-function getHexColor(){
-    let mColor = message.member.displayHexColor;
-    if(mColor == null){
-      return config.primary;
-    }else{
-      return mColor;
-    }
-   }
-  let user = message.author;
+const { Ecobase } = require('mongo.eco')
+const config = require('../config.json');
+const eco = new Ecobase(process.env.MONGO)
 
-  let timeout = 180000;
-  let amount = 5;
+module.exports.run = async (client, message, args) => {
 
-  let beg = await db.fetch(`beg_${message.guild.id}_${user.id}`);
+  const guildPrefix = await eco.fetch(`prefix_${message.guild.id}`)
+  if(guildPrefix === null) guildPrefix = config.prefix;
+if(!message.content.startsWith(guildPrefix))return;  
 
-  if (beg !== null && timeout - (Date.now() - beg) > 0) {
-    let time = ms(timeout - (Date.now() - beg));
-   
-    let timeEmbed = new Discord.MessageEmbed()
-    .setColor(getHexColor())
-    .setDescription(`:x: You've already collected your points!\n\nCollect again in ${time.minutes}m ${time.seconds}s `);
-    message.channel.send(timeEmbed)
-  } else {
-    let moneyEmbed = new Discord.MessageEmbed()
-  .setColor(getHexColor())
-  .setDescription(`:white_check_mark: You've collected  **${amount} points**`);
-  message.channel.send(moneyEmbed)
-  db.add(`money_${message.guild.id}_${user.id}`, amount)
-  db.set(`beg_${message.guild.id}_${user.id}`, Date.now())
+ // First send a message
+let m = await message.channel.send('collecting...')
 
+// Then setup the timeout in ms. 300000ms = 5 Mintes
+let timeout = 300000
 
+// Define eco.mongo as db
+const db = eco.mongo()
+
+// Fetch the time of last Beg...
+let lastBeg = await db.fetch(`lastBeg_${message.author.id}`)
+let vip = await db.fetch(`account_${message.author.id}`);
+function isPremiumUser(){
+  if(vip === true){
+    return '500';
   }
+  else{
+    return '200';
+  }
+}
+// Then setup the amount to give after beg...
+let amount = Math.floor(Math.random() * isPremiumUser())
+
+// If the user already claimed his beg
+if (lastBeg !== null && timeout - (Date.now() - lastBeg) > 0) {
+    let time = ms(timeout - (Date.now() - lastBeg));
+m.edit(`${config.error_icon} **You've already collected your rewards**`+ `\nClaim again after in ${time.minutes}m ${time.seconds}s`)
+}
+
+// If the user did not begged in 5 minutes...
+else{
+m.edit(`You've just collected  **${amount} coins**`)
+
+// Then add the amount to user's account...
+eco.add(message.author.id, amount)
+
+// Then set the cooldown...
+db.set(`lastBeg_${message.author.id}`, Date.now())
+
+}
+  
 };
 
 
 module.exports.help = {
     name: "collect",
-    type: "fun",
-    usage: "`collect `",
-    about: "collect points every 3 minutes :) ",
-    aliases: ["c"]
+    type:"fun",
+    usage:"`collect` or `c`",
+    about:"Collect coins in every 5 minutes",
+    aliases: ["beg", "c"]
   };
