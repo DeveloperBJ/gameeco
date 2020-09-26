5889fbd5aa0f3c680e3ccc9f6e0387aeb0943e16
DeveloperBJ <71545857+DeveloperBJ@users.noreply.github.com>
2020-09-26 18:43:25 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,53 +1,48 @@
+require('dotenv').config()
 const Discord = require('discord.js');
 const { Ecobase } = require('mongo.eco')
 const config = require('../config.json');
 const ms = require('parse-ms');
-const eco = new Ecobase(config.mongo_url)
-
+const eco = new Ecobase(process.env.MONGO)
 const db = eco.mongo();
-const Icon = [
-  "",
-  "https://i.imgur.com/NyQucPe.png",
-  "https://i.imgur.com/BsczYnp.png",
-  "https://i.imgur.com/WFRpoGL.png"
-];
 module.exports.run = async (bot, message, args) => {
   const guildPrefix = await eco.fetch(`prefix_${message.guild.id}`)
-  if(guildPrefix === null) guildPrefix = config.prefix;
-  if(!message.content.startsWith(guildPrefix))return;  
+  var prefix = (!guildPrefix) ? config.prefix : guildPrefix;
+  if (!message.content.startsWith(prefix)) return;
 
-  let command = prefix + module.exports.help.name + 1;
-  let msg = message.content.slice(command.length);
-
+  // let command = prefix + module.exports.help.name + 1;
+  // let msg = message.content.slice(command.length);
+  function getHexColor() {
+    let mColor = message.member.displayHexColor;
+    if (mColor == null) {
+      return config.primary;
+    } else {
+      return mColor;
+    }
+  }
   var cmdName = args[1];
   bot.commands.forEach(command => {
-    if (msg === command.help.name) {
+    if (args[0] === command.help.name) {
       var embed = new Discord.MessageEmbed()
+        .setTitle(`** ABOUT ${command.help.name.toUpperCase()} ━━━━**`)
         .setColor(getHexColor())
         .setDescription(`**Usage:** ${command.help.usage}\n**Description:** ${command.help.about}`);
       message.channel.send(embed);
     }
   });
-   function getHexColor(){
-    let mColor = message.member.displayHexColor;
-    if(mColor == null){
-      return botconfig.primary;
-    }else{
-      return mColor;
-    }
-   }
+
   if (msg === "") {
     let embed = new Discord.MessageEmbed()
       .setColor(getHexColor())
-      .addField(`Learn how to use a command ━━`, `1. ` + "To display command list - `"+ guildPrefix + "commands` \n " + "2. Learn more about a command - `"+ guildPrefix + "help [command_name]`")
+      .addField(`Learn how to use a command ━━`, `1. ` + "To display command list - `" + prefix + "commands` \n " + "2. Learn more about a command - `" + prefix + "help [command_name]`")
     message.channel.send(embed);
   }
 };
 
 module.exports.help = {
   name: "help",
-  type:"admin",
-  usage:"help <command_name>",
-  about:"Learn about a command",
-  aliases: [""]
+  type: "admin",
+  usage: "help <command_name>",
+  about: "Learn about a command",
+  aliases: ["learn", 'wiki', 'about']
 };
