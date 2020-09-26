f3c6188ef9d6c0396a7a80c60ae4646c01b445f2
DeveloperBJ <71545857+DeveloperBJ@users.noreply.github.com>
2020-09-26 18:32:53 +0530

    Update 1_start.js

--- commands/1_start.js
+++ commands/1_start.js
@@ -1,14 +1,17 @@
+require('dotenv').config()
+
 const ms = require('ms');
 const Discord = require('discord.js');
 const { Ecobase } = require('mongo.eco')
+const eco = new Ecobase(process.env.MONGO)
+
 const config = require('../config.json');
-const eco = new Ecobase(config.mongo_url)
 const db = eco.mongo();
 exports.run = async (bot, message, args) => {
 
     const guildPrefix = await eco.fetch(`prefix_${message.guild.id}`)
-    if(guildPrefix === null) guildPrefix = config.prefix;
-    if(!message.content.startsWith(guildPrefix))return;  
+  var prefix = (!guildPrefix) ? config.prefix : guildPrefix;
+    if(!message.content.startsWith(prefix))return;  
     // If the member doesn't have enough permissions
     if(!message.member.hasPermission('MANAGE_MESSAGES')){
         return message.channel.send(':x: You need to have the manage messages permissions to start giveaways.');
@@ -18,28 +21,28 @@ exports.run = async (bot, message, args) => {
     let giveawayChannel = message.mentions.channels.first();
     // If no channel is mentionned
     if(!giveawayChannel){
-        return message.channel.send(`${config.error_icon}  You have to mention a valid channel!`);
+        return message.channel.send(`:x:  You have to mention a valid channel!`);
     }
 
     // Giveaway duration
     let giveawayDuration = args[1];
     // If the duration isn't valid
     if(!giveawayDuration || isNaN(ms(giveawayDuration))){
-        return message.channel.send(`${config.error_icon}  You have to specify a valid duration!`);
+        return message.channel.send(`:x:  You have to specify a valid duration!`);
     }
 
     // Number of winners
     let giveawayNumberWinners = args[2];
     // If the specified number of winners is not a number
     if(isNaN(giveawayNumberWinners)){
-        return message.channel.send(`${config.error_icon} You have to specify a valid number of winners!`);
+        return message.channel.send(`:x: You have to specify a valid number of winners!`);
     }
 
     // Giveaway prize
     let giveawayPrize = args.slice(3).join(' ');
     // If no prize is specified
     if(!giveawayPrize){
-        return message.channel.send(`${config.error_icon} You have to specify a valid prize!`);
+        return message.channel.send(`:x: You have to specify a valid prize!`);
     }
 
     // Start the giveaway
@@ -75,10 +78,10 @@ exports.run = async (bot, message, args) => {
 
 };
 module.exports.help = {
-    name: "start-giveaway",
+    name: "start",
     type: "giveaway",
     usage: "`start-giveaway <channel> <duration> <winners> <prize> `\n"+
     `Example: start-giveaway #giveay-channel 6h 2 Pizza `,
     about: "To start a giveaway! ",
-    aliases: [""]
+    aliases: ["start-giveaway", "create-giveaway"]
   };
