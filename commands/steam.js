47667f3785ec406f7030543bd9127efc6901b6b8
DeveloperBJ <71545857+DeveloperBJ@users.noreply.github.com>
2020-10-10 18:34:21 +0530

    fixed embed

--- commands/steam.js
+++ commands/steam.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require('dotenv').config();
 const Discord = require("discord.js");
 const cheerio = require("cheerio");
 const axios = require("axios");
@@ -10,52 +10,45 @@ const db = eco.mongo();
 module.exports.run = async (bot, message, args) => {
   const guildPrefix = await eco.fetch(`prefix_${message.guild.id}`)
   var prefix = (!guildPrefix) ? config.prefix : guildPrefix;
-  if(!message.content.startsWith(prefix))return;    
-  if(!args[0]){
-    return message.channel.send('You have to specify the game name `'+ prefix +'data 2`')
+  // if (!message.content.startsWith(prefix)) return;
+  if (!args[0]) {
+    console.log('Please specify the game, like this `'+ prefix +'steam dota 2`')
   }
-  let command = prefix + module.exports.help.name + 1;
-  let msg = message.content.slice(command.length);
 
-  let WeaponName = message.content.slice(command.length);
+  let conName = prefix + module.exports.help.name + 1;
+
+  let gameName = message.content.slice(conName.length);
   axios
-    .get(`https://steamcharts.com/search/?q=${WeaponName}`)
+    .get(`https://steamcharts.com/search/?q=${gameName}`)
     .then(response => {
       const loadData = cheerio.load(response.data);
-      const WeaponContainer = loadData("table.common-table tbody tr");
+      const SteamContainer = loadData("table.common-table tbody tr");
       for (let i = 0; i < 1; i++) {
-        const ItemCon = loadData(WeaponContainer[i]).find("td.left a")[0];
-        const ItemAmmo = loadData(WeaponContainer[i]).find("td.right.num")[0];
-        const ItemIcon = loadData(WeaponContainer[i]).find("td img")[0];
-        if (ItemCon) {
-          const WeaponType = loadData(ItemCon).text();
-          const WeaponAmmo = loadData(ItemAmmo).text();
+        const ItemCon = loadData(SteamContainer[i]).find("td.left a")[0];
+        const curPlaying = loadData(SteamContainer[i]).find("td.right.num")[0];
+        const ItemIcon = loadData(SteamContainer[i]).find("td img")[0];
+        if(ItemIcon === null || !ItemIcon ){
+          message.channel.send(`**Rip:** No result found!`)
+        }
+        if (ItemIcon) {
+          const gameTitle = loadData(ItemCon).text();
+          const currentPlyers = loadData(curPlaying).text();
           const AppLink = loadData(ItemCon).attr("href");
           const AppIcon = loadData(ItemIcon).attr("src");
           const getLink = AppLink.slice(5);
           const getAppIcon = `https://steamcharts.com${AppIcon}`;
           const gameLink = `https://store.steampowered.com/app/${getLink}`;
-          function getHexColor(){
-            let mColor = message.member.displayHexColor;
-            if(mColor == null){
-              return config.primary;
-            }else{
-              return mColor;
-            }
-           }
-
           const WeaponEmbed = new Discord.MessageEmbed();
           // WeaponEmbed.setTitle(`:mag: Found result for ${WeaponName}`);
-          WeaponEmbed.setTitle(`**RESULT FOR ${WeaponName.toUpperCase()}** ━━━━`)
-          WeaponEmbed.setColor(getHexColor())
+          WeaponEmbed.setTitle(`**FOUND! ${gameTitle.toUpperCase()}** ━━━━`)
+          WeaponEmbed.setColor('BLUE')
           WeaponEmbed.setThumbnail(getAppIcon)
-          WeaponEmbed.addField("Title", WeaponType, true);
-          WeaponEmbed.addField("App ID", getLink, true);
+          WeaponEmbed.addField(":stop_sign: LIVE", `**${currentPlyers} PLAYERS ONLINE**`, true);
+          WeaponEmbed.addField("LINK", `[View on Steam](${gameLink})`, true);
+          WeaponEmbed.addField("HUB", `[Steam Community](https://steamcommunity.com/app/${getLink})`, false)
+          WeaponEmbed.setFooter(`Data provided by steam`);
+          WeaponEmbed.setTimestamp()
 
-          WeaponEmbed.addField("Playing now", `${WeaponAmmo} Players`, true);
-          
-          WeaponEmbed.setFooter(`Displaying data from Steam © 2020 ${bot.user.username}`);
-          
           message.channel.startTyping();
 
           setTimeout(() => {
@@ -65,10 +58,9 @@ module.exports.run = async (bot, message, args) => {
           }, 2000);
           // console.log(WeaponIcon);
         }
-      
       }
     });
-  
+
 };
 
 module.exports.help = {
