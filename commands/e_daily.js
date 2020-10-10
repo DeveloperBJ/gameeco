720d9a772191c5331ab3df78d341de29f4a0111c
DeveloperBJ <71545857+DeveloperBJ@users.noreply.github.com>
2020-10-10 18:15:53 +0530

    Update e_daily.js

--- commands/e_daily.js
+++ commands/e_daily.js
@@ -7,49 +7,49 @@ const eco = new Ecobase(process.env.MONGO)
 
 module.exports.run = async (client, message, args) => {
   const guildPrefix = await eco.fetch(`prefix_${message.guild.id}`)
-      var prefix = (!guildPrefix) ? config.prefix : guildPrefix;
-    if(!message.content.startsWith(prefix))return;   
+  var prefix = (!guildPrefix) ? config.prefix : guildPrefix;
+  if (!message.content.startsWith(prefix)) return;
 
- // First send a message
-let m = await message.channel.send('collecting...')
+  // First send a message
+  let m = await message.channel.send('collecting...')
 
-// Then setup the timeout in ms. 300000ms = 5 Mintes
-let timeout = 8.64e+7
+  // Then setup the timeout in ms. 300000ms = 5 Mintes
+  let timeout = 8.64e+7
 
-// Define eco.mongo as db
-const db = eco.mongo()
+  // Define eco.mongo as db
+  const db = eco.mongo()
 
-// Fetch the time of last Beg...
-let lastBeg = await db.fetch(`lastDaily_${message.author.id}`)
+  // Fetch the time of last Beg...
+  let lastBeg = await db.fetch(`lastDaily_${message.author.id}`)
 
-// Then setup the amount to give after beg...
-let amount = Math.floor(Math.random() * 800)
+  // Then setup the amount to give after beg...
+  let amount = Math.floor(Math.random() * 800)
 
-// If the user already claimed his beg
-if (lastBeg !== null && timeout - (Date.now() - lastBeg) > 0) {
+  // If the user already claimed his beg
+  if (lastBeg !== null && timeout - (Date.now() - lastBeg) > 0) {
     let time = ms(timeout - (Date.now() - lastBeg));
-m.edit(`${config.error_icon} **You've already collected your daily rewards**` + `\nClaim again in ${time.hours}h ${time.minutes}m ${time.seconds}s`)
-}
+   await m.edit(`${config.error_icon} **You've already collected your daily rewards**` + `\nClaim again in ${time.hours}h ${time.minutes}m ${time.seconds}s`)
+  }
 
-// If the user did not begged in 5 minutes...
-else{
-m.edit(`You've just collected  **${amount} coins**`)
+  // If the user did not begged in 5 minutes...
+  else {
+   await m.edit(`You've just collected your daily  **${amount} coins**`)
 
-// Then add the amount to user's account...
-eco.add(message.author.id, amount)
+    // Then add the amount to user's account...
+    eco.add(message.author.id, amount)
 
-// Then set the cooldown...
-db.set(`lastDaily_${message.author.id}`, Date.now())
+    // Then set the cooldown...
+    db.set(`lastDaily_${message.author.id}`, Date.now())
 
-}
-  
+  }
+
 };
 
 
 module.exports.help = {
-    name: "daily",
-    type:"fun",
-    usage:"`daily` or `day`",
-    about:"To collect daily coins!",
-    aliases: ["d", "day"]
-  };
+  name: "daily",
+  type: "fun",
+  usage: "`daily` or `day`",
+  about: "To collect daily coins!",
+  aliases: ["d", "day"]
+};
