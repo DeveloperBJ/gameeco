8684e8cb49ad27096862b0fd8154bc30247ee072
DeveloperBJ <71545857+DeveloperBJ@users.noreply.github.com>
2020-10-10 18:12:05 +0530

    Update commands.js

--- commands/commands.js
+++ commands/commands.js
@@ -11,7 +11,6 @@ module.exports.run = async (bot, message, args) => {
   const guildPrefix = await eco.fetch(`prefix_${message.guild.id}`)
   var prefix = (!guildPrefix) ? config.prefix : guildPrefix;
     if(!message.content.startsWith(prefix))return;  
-  let command = prefix + module.exports.help.name + 1;
   var generalCommands = "";
   var gamesCommands = "";
   var funCommands = "";
@@ -40,6 +39,23 @@ module.exports.run = async (bot, message, args) => {
       // ignore hidden commands :^)
     }
   });
+  function gamesCategory(){
+    if(gamesCommands === null){
+      return 'There is no command!';
+    }else{
+      return gamesCommands;
+    }
+  }
+
+  function mainCategory(){
+    if(generalCommands === null){
+      return 'There is no command!';
+    }else{
+      return generalCommands;
+    }
+  }
+
+
   function getHexColor(){
     let mColor = message.member.displayHexColor;
     if(mColor == null){
@@ -48,23 +64,18 @@ module.exports.run = async (bot, message, args) => {
       return mColor;
     }
    }
+   let gCommands = (giveawayCommands) ? giveawayCommands : '` Empty category! `'
 
   let embed = new Discord.MessageEmbed()
-    .setTitle(`${bot.user.username.toUpperCase()}'S COMMAND LIST ━━━━━`,)
+    .setTitle(`**MY COMMAND DIRECTORY** ━━━━━`,)
+    .setThumbnail(bot.user.displayAvatarURL())
     .setColor(getHexColor())
-    .setDescription( ` To learn how to use a command for example ` + "`f!help [command_name]` For more settings & customization - `f!set`"+
-      '\n\n:white_small_square: **Some general and useful commands** \n'+
-    generalCommands +
-    '\n\n :white_small_square: **Play games earn coins; commands** \n' +
-    gamesCommands +
-    '\n\n :white_small_square: **Do giveaway by these commands [beta]** \n' +
-    giveawayCommands +
-    '\n\n :white_small_square: **Some fun and economy commands** \n' +
-    funCommands +
-    '\n\n :white_small_square: **Some admin and useless commands** \n' +
-     adminCommands +
-    '\n\n :white_small_square: **For custom bot prefix for this guild** \n' +
-     '`f!set prefix <prefix>`')
+     .addField(`**GENERAL**`, `${mainCategory()}`, false)
+     .addField(`**GAMES**`, `${gamesCategory()}`, true)
+     .addField(`**Giveaway**`, `${gCommands}`, true)
+     .addField(`**Fun**`, `${funCommands}`, false)
+     .addField(`**Admin**`, `${adminCommands}`, true)
+
      .setFooter(`Requested by ${message.author.tag}`)
      .setTimestamp()
   message.channel.startTyping();
@@ -76,9 +87,9 @@ module.exports.run = async (bot, message, args) => {
 };
 
 module.exports.help = {
-  name: "cmds",
-  type: "general",
+  name: "commands",
+  type: "admin",
   usage: "`commands` or `cmds` or `all`",
   about: "To display command list",
-  aliases: ["commands", 'botcommands', 'all']
+  aliases: ["cmds", 'botcommands', 'all']
 };
